02f94709ae9068fb1f03a82e2dec526b9dff4cb1
unknown <davidbonnerar@gmail.com>
2020-11-23 00:24:28 -0300

    Tela de pesquisa de imagens e vídeos adicionda com activity

--- app/src/main/java/com/example/cyberspace_info/pesquisarimgvid/PesquisaImgVidActivity.kt
+++ app/src/main/java/com/example/cyberspace_info/pesquisarimgvid/PesquisaImgVidActivity.kt
@@ -2,11 +2,44 @@ package com.example.cyberspace_info.pesquisarimgvid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.viewpager.widget.ViewPager
 import com.example.cyberspace_info.R
+import com.google.android.material.tabs.TabLayout
+import java.lang.reflect.Array.newInstance
+import android.widget.TableLayout
 
 class PesquisaImgVidActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pesquisa_img_vid)
+
+        findViewById<ImageView>(R.id.imgViewMenuPesquisaImgVid).setOnClickListener {
+            onBackPressed()
+        }
+
+        val pager = findViewById<ViewPager>(R.id.viewPager)
+        val tab = findViewById<TabLayout>(R.id.tabLayout)
+
+        //Faz com que o tab use o Viewpager
+        tab.setupWithViewPager(pager)
+
+        val fragments = listOf(
+            ResultadosPesquisaFragment.newInstance("Minhas Imagens pesquisadas", true),
+            ResultadosPesquisaFragment.newInstance("Meus Vídeos pesquisados",false)
+        )
+
+        val titulos = listOf(
+            getString(R.string.imagens), getString(R.string.videos)
+        )
+
+        pager.adapter = ViewPagerAdapter(fragments, titulos, supportFragmentManager)
+
+
+    }
+
+    override fun finish() {
+        super.finish()
+        overridePendingTransition(R.anim.from_left,R.anim.to_right)
     }
 }
--- app/src/main/java/com/example/cyberspace_info/pesquisarimgvid/ResultadosPesquisaFragment.kt
+++ app/src/main/java/com/example/cyberspace_info/pesquisarimgvid/ResultadosPesquisaFragment.kt
@@ -5,16 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.example.cyberspace_info.R
 
 class ResultadosPesquisaFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: Boolean? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString("TEXTO")
+            param2 = it.getBoolean("EH_MAIOR_DE_IDADE")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resultados_pesquisa, container, false)
+        val minhaView = inflater.inflate(R.layout.fragment_resultados_pesquisa, container, false)
+        minhaView.findViewById<TextView>(R.id.txtMeuTexto).text = param1
+
+        return minhaView
     }
 
+    companion object {
+        @JvmStatic
+        fun newInstance(texto: String, ehMarioDeIdade: Boolean) =
+            ResultadosPesquisaFragment().apply {
+                arguments = Bundle().apply {
+                    putString("TEXTO", texto)
+                    putString("EH_MAIOR_DE_IDADE", texto)
+                }
+            }
+    }
 }
