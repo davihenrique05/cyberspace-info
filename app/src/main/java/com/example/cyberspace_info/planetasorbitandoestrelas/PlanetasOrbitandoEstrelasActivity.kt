0af191f71aa3790acc2144a0d125d08a8896c6b7
unknown <davidbonnerar@gmail.com>
2020-11-23 12:03:12 -0300

    Alguns acertos não importantes

---
+++ app/src/main/java/com/example/cyberspace_info/planetasorbitandoestrelas/PlanetasOrbitandoEstrelasActivity.kt
@@ -0,0 +1,11 @@
+package com.example.cyberspace_info.planetasorbitandoestrelas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PlanetasOrbitandoEstrelasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_planetas_orbitando_estrelas)
+    }
+}
