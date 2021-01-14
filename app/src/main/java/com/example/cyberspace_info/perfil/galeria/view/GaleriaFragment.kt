15c2700d612d6951d258fe506245d62fd08b2748
Davi <davihgs20@gmail.com>
2021-01-14 18:28:13 -0300

    Adcionando galeria com as imagens favoritas do DB

--- app/src/main/java/com/example/cyberspace_info/perfil/galeria/view/GaleriaFragment.kt
+++ app/src/main/java/com/example/cyberspace_info/perfil/galeria/view/GaleriaFragment.kt
@@ -1,21 +1,30 @@
 package com.example.cyberspace_info.perfil.galeria.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.core.os.bundleOf
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.observe
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cyberspace_info.R
+import com.example.cyberspace_info.db.ImagemDatabase
+import com.example.cyberspace_info.perfil.entity.ImagemEntity
 import com.example.cyberspace_info.perfil.galeria.view.adapter.ImagensAdapter
+import com.example.cyberspace_info.perfil.repository.ImagemRepository
+import com.example.cyberspace_info.perfil.viewmodel.ImagemViewModel
 
 
 class GaleriaFragment : Fragment() {
 
+    lateinit var _viewModel: ImagemViewModel
+    lateinit var _recylerAdapter: ImagensAdapter
+    private var _listaDeImagens = mutableListOf<String>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,27 +38,26 @@ class GaleriaFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val recyler = view.findViewById<RecyclerView>(R.id.recyclerViewGaleria)
         val manager = GridLayoutManager(view.context, 3)
-        val back = view.findViewById<ImageView>(R.id.imageIconReturnGaleria)
         val tela = arguments?.getString("Origem")
 
-        lateinit var listaDeImagens: MutableList<String>
-        lateinit var recylerAdapter: ImagensAdapter
+
+        _viewModel = ViewModelProvider(
+            this,
+            ImagemViewModel.ImagemViewModelFacytory(
+                ImagemRepository(
+                    ImagemDatabase.getDataBase(view.context).imagemDao()
+                )
+            )
+        ).get(ImagemViewModel::class.java)
 
         if (tela == "Perfil") {
 
-            recylerAdapter = ImagensAdapter(listaDeImagens, true) {
-                val navController = Navigation.findNavController(view)
-                val bundle = bundleOf(
-                    "Tela" to getString(R.string.galeria_comparacao),
-                    "Imagem" to it.toInt(),
-                    "Origem" to getString(R.string.perfil_comparacao)
-                )
-                navController.navigate(R.id.action_galeriaFragment_to_imagemFragment, bundle)
-            }
-        } else if(tela == "MarsRover") {
-            listaDeImagens = arguments?.get("imagens") as MutableList<String>
-            recylerAdapter = ImagensAdapter(listaDeImagens, false) {
-                val navController = Navigation.findNavController(view)
+            carregarImagensFavoritas()
+
+        } else if (tela == "MarsRover") {
+            _listaDeImagens = arguments?.get("imagens") as MutableList<String>
+            _recylerAdapter = ImagensAdapter(_listaDeImagens) {
+                val navController = Navigation.findNavController(requireView())
                 val bundle = bundleOf(
                     "Tela" to getString(R.string.galeria_comparacao),
                     "Imagem" to it,
@@ -61,18 +69,56 @@ class GaleriaFragment : Fragment() {
 
         recyler.apply {
             setHasFixedSize(true)
-            adapter = recylerAdapter
+            adapter = _recylerAdapter
             layoutManager = manager
         }
 
+        navegacaoEntreTelas(tela)
+    }
+
+
+    fun carregarImagensFavoritas() {
+
+        _recylerAdapter = ImagensAdapter(_listaDeImagens) {
+            val navController = Navigation.findNavController(requireView())
+            val bundle = bundleOf(
+                "Tela" to getString(R.string.galeria_comparacao),
+                "Imagem" to it,
+                "Origem" to getString(R.string.perfil_comparacao)
+            )
+            navController.navigate(R.id.action_galeriaFragment_to_imagemFragment, bundle)
+        }
+
+        _viewModel.obterImagems().observe(viewLifecycleOwner) {
+            val listaUrl = extrairUrl(it)
+            _listaDeImagens.clear()
+            _listaDeImagens.addAll(listaUrl)
+            _recylerAdapter.notifyDataSetChanged()
+        }
+    }
+
+    fun navegacaoEntreTelas(tela: String?) {
+
+        val back = requireView().findViewById<ImageView>(R.id.imageIconReturnGaleria)
         back.setOnClickListener {
-            val navegar = Navigation.findNavController(view)
+            val navegar = Navigation.findNavController(requireView())
             if (tela == "Perfil") {
                 navegar.navigate(R.id.action_galeriaFragment_to_perfilFragment)
-            }else {
+            } else {
                 navegar.navigate(R.id.action_galeriaFragment_to_marsRoverFragment)
             }
         }
     }
 
+    fun extrairUrl(lista: MutableList<ImagemEntity>): MutableList<String> {
+        val listaDeUrl = mutableListOf<String>()
+        val ultimo = lista.size - 1
+
+        for (i in ultimo downTo 0) {
+            val imagem = lista[i]
+            listaDeUrl.add(imagem.url)
+        }
+
+        return listaDeUrl
+    }
 }
--- app/src/main/java/com/example/cyberspace_info/perfil/galeria/view/adapter/ImagensAdapter.kt
+++ app/src/main/java/com/example/cyberspace_info/perfil/galeria/view/adapter/ImagensAdapter.kt
@@ -8,31 +8,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.cyberspace_info.R
 import com.squareup.picasso.Picasso
 
-class ImagensAdapter(private val listaDeImagens: MutableList<String>, private val isImagemInt:Boolean, private val listener: (String) -> Unit):RecyclerView.Adapter<ImagensAdapter.ImagensViewHolder>() {
+class ImagensAdapter(
+    private val listaDeImagens: MutableList<String>,
+    private val listener: (String) -> Unit
+) : RecyclerView.Adapter<ImagensAdapter.ImagensViewHolder>() {
 
-    class ImagensViewHolder(view: View):RecyclerView.ViewHolder(view) {
+    class ImagensViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
         private val imagem = view.findViewById<ImageView>(R.id.imageViewItem)
 
-        fun bind(url:String, isInt:Boolean){
-            if (isInt) {
-                imagem.setImageResource(url.toInt())
-            } else {
-                Picasso.get()
-                    .load(url)
-                    .into(imagem)
-            }
+        fun bind(url: String) {
+            Picasso.get()
+                .load(url)
+                .into(imagem)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImagensViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_lista_imagens, parent,false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_lista_imagens, parent, false)
 
         return ImagensViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ImagensViewHolder, position: Int) {
         val item = listaDeImagens[position]
-        holder.bind(item,isImagemInt)
+        holder.bind(item)
         holder.itemView.setOnClickListener { listener(item) }
     }
 
