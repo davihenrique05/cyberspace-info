687465c39f4756f83b64ae4652ead6b7f4804f3e
Genielson <ge.nielson@hotmail.com>
2021-02-08 21:12:43 -0300

    Adicionando paginacao com sleep em Pesquisa de Imagens

--- app/src/main/java/com/grupo5/cyberspace/pesquisarimgvid/view/ResultadoPesquisaFragment.kt
+++ app/src/main/java/com/grupo5/cyberspace/pesquisarimgvid/view/ResultadoPesquisaFragment.kt
@@ -2,6 +2,7 @@ package com.grupo5.cyberspace.pesquisarimgvid.view
 
 import android.content.Context
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,19 +19,24 @@ import androidx.lifecycle.observe
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.textfield.TextInputEditText
 import com.grupo5.cyberspace.R
 import com.grupo5.cyberspace.pesquisarimgvid.model.ObjectImageModel
 import com.grupo5.cyberspace.pesquisarimgvid.repository.PesquisarImagemRepository
 import com.grupo5.cyberspace.pesquisarimgvid.view.adapter.PesquisaImgAdapter
 import com.grupo5.cyberspace.pesquisarimgvid.viewmodel.PesquisarImagemViewModel
-import com.google.android.material.card.MaterialCardView
-import com.google.android.material.textfield.TextInputEditText
 
 class ResultadoPesquisaFragment : Fragment() {
 
     private lateinit var _viewModel: PesquisarImagemViewModel
     private lateinit var _listaImagens: MutableList<ObjectImageModel>
     private lateinit var _adaptador: PesquisaImgAdapter
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var _viewManager : GridLayoutManager
+    private var currentItens = 0
+    private var totalItens = 0
+    private var scrollOutItens = 0
+    var lastItemCompare = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,7 +44,6 @@ class ResultadoPesquisaFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_resultado_pesquisa, container, false)
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -48,7 +53,9 @@ class ResultadoPesquisaFragment : Fragment() {
 
         resultarPesquisa(search)
         view.findViewById<ImageView>(R.id.imgViewMenuPesquisaImgVid).setOnClickListener {
+            TOTAL_INITAL_ITENS_IMAGE = 0
             navigation.popBackStack()
+
         }
         if(!search.isNullOrEmpty()){
             requireView().findViewById<TextInputEditText>(R.id.txtpesquisaimagefragment).setText(search)
@@ -68,7 +75,7 @@ class ResultadoPesquisaFragment : Fragment() {
             return@setOnEditorActionListener true
         }
         requireView().findViewById<ImageView>(R.id.imgViewProcurarPesquisaImgVid).setOnClickListener {
-                mudarTela(search.text.toString())
+            mudarTela(search.text.toString())
         }
     }
 
@@ -93,8 +100,8 @@ class ResultadoPesquisaFragment : Fragment() {
             )
         ).get(PesquisarImagemViewModel::class.java)
 
-        val viewManager = GridLayoutManager(requireContext(), 3)
-        val recyclerView = requireView().findViewById<RecyclerView>(R.id.listaFotosVideos)
+        _viewManager = GridLayoutManager(requireContext(), 3)
+        recyclerView = requireView().findViewById<RecyclerView>(R.id.listaFotosVideos)
         _adaptador = PesquisaImgAdapter(_listaImagens) {
             val navigation = Navigation.findNavController(requireView())
             val bundle = bundleOf("Imagem" to it.links[0].href)
@@ -103,7 +110,7 @@ class ResultadoPesquisaFragment : Fragment() {
 
         recyclerView.apply {
             setHasFixedSize(true)
-            layoutManager = viewManager
+            layoutManager = _viewManager
             adapter = _adaptador
         }
 
@@ -133,9 +140,72 @@ class ResultadoPesquisaFragment : Fragment() {
 
 
     private fun exibirLista(lista: List<ObjectImageModel>) {
-        _listaImagens.addAll(lista)
+
+        for (i in START_VALUE_LIST_IMAGE..INDEX_START_VALUE_IMAGE) {
+            _listaImagens.add(lista[i])
+            _adaptador.notifyDataSetChanged()
+        }
+
+        TOTAL_INITAL_ITENS_IMAGE = TOTAL_INITAL_ITENS_IMAGE + ITENS_AFTER_UPDATE_IMAGE
+
         showLoading(false)
-        _adaptador.notifyDataSetChanged()
+
+        var firstVisible = _viewManager.findFirstVisibleItemPosition()
+
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+
+            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+                super.onScrollStateChanged(recyclerView, newState)
+
+            }
+
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val currentFirstVisible = _viewManager.findFirstVisibleItemPosition()
+
+                firstVisible = currentFirstVisible
+
+                val beforeScrollTotal = START_VALUE_LIST_IMAGE
+
+                currentItens = _viewManager.childCount
+                totalItens = _viewManager.itemCount
+                scrollOutItens = _viewManager.findFirstVisibleItemPosition()
+
+                pushDinamicItensWithSleep(dy,beforeScrollTotal,lista)
+
+            }
+
+        })
+
+    }
+
+    private fun pushDinamicItensWithSleep(dy:Int,beforeScrollTotal:Int,lista: List<ObjectImageModel>){
+
+        val lastItemCurrent = _viewManager.findLastVisibleItemPosition()
+
+        if(!(dy < START_VALUE_LIST_IMAGE && scrollOutItens >= START_VALUE_LIST_IMAGE && beforeScrollTotal == START_VALUE_LIST_IMAGE)){
+
+            if(lastItemCurrent >= lastItemCompare){
+
+                lastItemCompare = lastItemCurrent
+
+                if((lastItemCompare <=  lista.size) && (lastItemCompare) >= (TOTAL_INITAL_ITENS_IMAGE)){
+
+                    SystemClock.sleep(1500)
+
+                    for (i in (lastItemCompare)..(lastItemCompare+ITENS_AFTER_UPDATE_IMAGE)) {
+
+                        if(i <= lista.size-1) {
+                            _listaImagens.add(lista[i])
+                        }
+                    }
+                    TOTAL_INITAL_ITENS_IMAGE = TOTAL_INITAL_ITENS_IMAGE + ITENS_AFTER_UPDATE_IMAGE
+                }
+            }
+
+        }
+
     }
 
     private fun View.hideKeyboard() {
@@ -149,4 +219,12 @@ class ResultadoPesquisaFragment : Fragment() {
         _listaImagens = mutableListOf()
         resultarPesquisa(search)
     }
+
+    companion object{
+        var TOTAL_INITAL_ITENS_IMAGE = 0
+        var ITENS_AFTER_UPDATE_IMAGE = 25
+        var START_VALUE_LIST_IMAGE = 0
+        var INDEX_START_VALUE_IMAGE = 25
+    }
+
 }
