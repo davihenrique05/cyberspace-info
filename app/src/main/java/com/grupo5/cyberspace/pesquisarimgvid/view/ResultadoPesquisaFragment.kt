17744c16d4997849b702b5df414675eb7f4ff56b
Davi <davihgs20@gmail.com>
2021-02-09 20:37:29 -0300

    Pequenas alterações em Resultados Fragment

--- app/src/main/java/com/grupo5/cyberspace/pesquisarimgvid/view/ResultadoPesquisaFragment.kt
+++ app/src/main/java/com/grupo5/cyberspace/pesquisarimgvid/view/ResultadoPesquisaFragment.kt
@@ -10,10 +10,12 @@ import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageView
 import android.widget.ProgressBar
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.observe
 import androidx.navigation.Navigation
@@ -33,11 +35,7 @@ class ResultadoPesquisaFragment : Fragment() {
     private lateinit var _adaptador: PesquisaImgAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var _viewManager : GridLayoutManager
-    private var currentItens = 0
-    private var totalItens = 0
-    private var scrollOutItens = 0
-    var lastItemCompare = 0
-
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -48,17 +46,17 @@ class ResultadoPesquisaFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val navigation = Navigation.findNavController(view)
-        val search = arguments?.getString("search")
+        if(_search.isBlank()){
+            _search = arguments?.getString("search").toString()
+        }
         _listaImagens = mutableListOf()
-
-        resultarPesquisa(search)
+        resultarPesquisa(_search)
         view.findViewById<ImageView>(R.id.imgViewMenuPesquisaImgVid).setOnClickListener {
-            TOTAL_INITAL_ITENS_IMAGE = 0
             navigation.popBackStack()
 
         }
-        if(!search.isNullOrEmpty()){
-            requireView().findViewById<TextInputEditText>(R.id.txtpesquisaimagefragment).setText(search)
+        if(!_search.isNullOrEmpty()){
+            requireView().findViewById<TextInputEditText>(R.id.txtpesquisaimagefragment).setText(_search)
             requireView().findViewById<TextInputEditText>(R.id.txtpesquisaimagefragment).clearFocus()
         }
         novaPesquisa()
@@ -70,12 +68,14 @@ class ResultadoPesquisaFragment : Fragment() {
         search.setOnEditorActionListener { _, action, event ->
             if(action == EditorInfo.IME_ACTION_SEARCH)
             {
-                mudarTela(search.text.toString())
+                _search = search.text.toString()
+                mudarTela(_search)
             }
             return@setOnEditorActionListener true
         }
         requireView().findViewById<ImageView>(R.id.imgViewProcurarPesquisaImgVid).setOnClickListener {
-            mudarTela(search.text.toString())
+            _search = search.text.toString()
+            mudarTela(_search)
         }
     }
 
@@ -141,74 +141,12 @@ class ResultadoPesquisaFragment : Fragment() {
 
     private fun exibirLista(lista: List<ObjectImageModel>) {
 
-        for (i in START_VALUE_LIST_IMAGE..INDEX_START_VALUE_IMAGE) {
-            _listaImagens.add(lista[i])
-            _adaptador.notifyDataSetChanged()
-        }
-
-        if(CLICK_SEARCH == false){
-        TOTAL_INITAL_ITENS_IMAGE = TOTAL_INITAL_ITENS_IMAGE + ITENS_AFTER_UPDATE_IMAGE
-}
-
         showLoading(false)
-
-        var firstVisible = _viewManager.findFirstVisibleItemPosition()
-
-        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener(){
-
-            override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
-                super.onScrollStateChanged(recyclerView, newState)
-
-            }
-
-            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
-                super.onScrolled(recyclerView, dx, dy)
-
-                val currentFirstVisible = _viewManager.findFirstVisibleItemPosition()
-
-                firstVisible = currentFirstVisible
-
-                val beforeScrollTotal = START_VALUE_LIST_IMAGE
-
-                currentItens = _viewManager.childCount
-                totalItens = _viewManager.itemCount
-                scrollOutItens = _viewManager.findFirstVisibleItemPosition()
-
-                pushDinamicItensWithSleep(dy,beforeScrollTotal,lista)
-
-            }
-
-        })
-
+        _listaImagens.clear()
+        _listaImagens.addAll(lista)
+        _adaptador.notifyDataSetChanged()
     }
 
-    private fun pushDinamicItensWithSleep(dy:Int,beforeScrollTotal:Int,lista: List<ObjectImageModel>){
-
-        val lastItemCurrent = _viewManager.findLastVisibleItemPosition()
-
-        if(!(dy < START_VALUE_LIST_IMAGE && scrollOutItens >= START_VALUE_LIST_IMAGE && beforeScrollTotal == START_VALUE_LIST_IMAGE)){
-
-            if(lastItemCurrent >= lastItemCompare){
-
-                lastItemCompare = lastItemCurrent
-
-                if((lastItemCompare <=  lista.size) && (lastItemCompare) >= (TOTAL_INITAL_ITENS_IMAGE)){
-
-                    SystemClock.sleep(1500)
-
-                    for (i in (lastItemCompare)..(lastItemCompare+ITENS_AFTER_UPDATE_IMAGE)) {
-
-                        if(i <= lista.size-1) {
-                            _listaImagens.add(lista[i])
-                        }
-                    }
-                    TOTAL_INITAL_ITENS_IMAGE = TOTAL_INITAL_ITENS_IMAGE + ITENS_AFTER_UPDATE_IMAGE
-                }
-            }
-
-        }
-
-    }
 
     private fun View.hideKeyboard() {
         val inputManager =
@@ -220,18 +158,10 @@ class ResultadoPesquisaFragment : Fragment() {
         view?.hideKeyboard()
         _listaImagens = mutableListOf()
         resultarPesquisa(search)
-        lastItemCompare = 0
-        CLICK_SEARCH = true
-        TOTAL_INITAL_ITENS_IMAGE = 0
-        resultarPesquisa(search)
     }
 
     companion object{
-        var CLICK_SEARCH = false
-        var TOTAL_INITAL_ITENS_IMAGE = 0
-        var ITENS_AFTER_UPDATE_IMAGE = 25
-        var START_VALUE_LIST_IMAGE = 0
-        var INDEX_START_VALUE_IMAGE = 25
+        var _search = ""
     }
 
 }
